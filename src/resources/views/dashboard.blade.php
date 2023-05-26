e776ab48cbe48125a76fbcf771976aae053aa4bf
yuita-yoshihiko <soup998211@gmail.com>
2023-05-26 19:56:32 +0900

    ファイル名の変更

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -5,10 +5,6 @@
       <div class="text-center lg:w-2/3 w-full">
         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">あとで</h1>
         <p class="mb-8 leading-relaxed">あとでは、あとでやろう！と思っていることを一括管理できるアプリです。</p>
-        <div class="flex justify-center">
-          <a href="{{ route('login') }}"  class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">ログイン</a>
-          <a href="{{ route('register') }}" class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">ユーザー登録</a>
-        </div>
       </div>
     </div>
   </section>
--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home.index');
+    return view('dashboard');
 });
 
 Route::get('/dashboard', function () {
