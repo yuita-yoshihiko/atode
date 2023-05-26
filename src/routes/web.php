f537ee93bebaa361dff1b105a11390bfb8de559c
yuita-yoshihiko <soup998211@gmail.com>
2023-05-26 22:27:26 +0900

    task関連のファイル作成とルーティングの定義

--- src/routes/web.php
+++ src/routes/web.php
@@ -28,4 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::resource('tasks', TaskController::class);
+
 require __DIR__.'/auth.php';
